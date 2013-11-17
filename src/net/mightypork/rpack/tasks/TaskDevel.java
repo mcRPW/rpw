1e224f6e7b77dd34abd4ab8532802be2e537e300
MightyPork <ondra@ondrovo.com>
2013-11-17 01:21:26 +0100

    dev task

--- src/net/mightypork/rpack/tasks/TaskDevel.java
+++ src/net/mightypork/rpack/tasks/TaskDevel.java
@@ -1,9 +1,35 @@
 package net.mightypork.rpack.tasks;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import net.mightypork.rpack.struct.PackInfo;
+import net.mightypork.rpack.struct.PackInfoMap;
+import net.mightypork.rpack.struct.SoundEntry;
+import net.mightypork.rpack.struct.SoundEntryMap;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 
 public class TaskDevel {
 
 	public static void run() {
+		
+//		SoundEntryMap map = SoundEntryMap.fromJson("{"
+//				+ "\"a\":   {\"category\": \"neutral\",\"sounds\": [\"custom_sounds/test\"]},"
+//				+ "\"b\":  {\"category\": \"dfgdf\",\"sounds\": [\"BBB\",\"FUCK YOU BITCH!\"]}"
+//				+ "}");*/
+//		
+//		/*System.out.println(map.get("a").getClass());
+//		System.out.println(map.toJson());
+		
+//		PackInfoMap pim = new PackInfoMap();
+//		pim.setPackInfo(new PackInfo(1, "He/ll\"\"\".,!@## yeah!"));
+//		
+//		System.out.println(pim.toJson());
+		
+		
 		
 	}
 }
