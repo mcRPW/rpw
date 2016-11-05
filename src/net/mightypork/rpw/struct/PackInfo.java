cba45ec6b8b1ea23eb0f62be29b7bfd8445dc4d6
MCrafterzz <marcusaccounts@yahoo.se>
2016-11-05 12:32:49 +0100

    PackInfo now saves title

--- src/net/mightypork/rpw/struct/PackInfo.java
+++ src/net/mightypork/rpw/struct/PackInfo.java
@@ -5,21 +5,20 @@ public class PackInfo
 
 	public int pack_format;
 	public String description;
-
+	public String title;
 
 	public PackInfo() {
 	}
 
-
-	public PackInfo(int format, String desc) {
+	public PackInfo(int format, String title, String desc) {
 		this.pack_format = format;
+		this.title = title;
 		this.description = desc;
 	}
-
 
 	@Override
 	public String toString()
 	{
-		return "PACK[format: " + pack_format + ", description: " + description + "]";
+		return "PACK[format: " + pack_format + ", title: " + title + ", description: " + description + "]";
 	}
 }
