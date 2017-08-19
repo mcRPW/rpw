d510dbda6db3a79bee986aa354cba5e8c6dc4569
MCrafterzz <marcusaccounts@yahoo.se>
2017-08-19 10:10:06 +0200

    Update version number

--- src/net/mightypork/rpw/Const.java
+++ src/net/mightypork/rpw/Const.java
@@ -13,7 +13,7 @@ import com.google.gson.GsonBuilder;
 
 
 public class Const {
-    public static final int VERSION_SERIAL = 432;
+    public static final int VERSION_SERIAL = 433;
 
     public static final String VERSION = VersionUtils.getVersionString(VERSION_SERIAL);
     //public static final int VERSION_MAJOR = VersionUtils.getVersionMajor(VERSION_SERIAL);
